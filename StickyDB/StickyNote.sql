8c855e9869217d4d2dd74b384e902d8bcb78ba8d
= <michaelstvns2@gmail.com>
2024-06-29 20:37:00 +0100

    set id to auto increment by 1

--- StickyDB/StickyNote.sql
+++ StickyDB/StickyNote.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[StickyNote]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Title] NVARCHAR(50) NOT NULL, 
     [Category] NVARCHAR(50) NOT NULL, 
     [CreatedDate] DATETIME NULL, 
