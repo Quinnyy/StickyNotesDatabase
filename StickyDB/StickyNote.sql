abd34c2d1ddc1171d5049f569eceda745890d8fc
= <michaelstvns2@gmail.com>
2024-06-29 17:54:17 +0100

    Add project files.

---
+++ StickyDB/StickyNote.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[StickyNote]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Category] NVARCHAR(50) NOT NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [Description] NVARCHAR(500) NULL, 
+    [Updated] DATETIME NULL
+)
